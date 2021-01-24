d7b8dbfbeff201ed2d61d2ad81ad3444e5fe73a0
bonny-412 <bonny-412@users.noreply.github.com>
2021-01-24 23:13:41 +0100

    Commit

--- js/main.js
+++ js/main.js
@@ -1,30 +1,5 @@
 (function($) {
-    "use strict";
-        // Portfolio subpage filters
-        function portfolio_init() {
-            var portfolio_grid = $('.portfolio-grid'),
-                portfolio_filter = $('.portfolio-filters');
-                
-            if (portfolio_grid) {
-    
-                portfolio_grid.shuffle({
-                    speed: 450,
-                    itemSelector: 'figure'
-                });
-    
-                portfolio_filter.on("click", ".filter", function (e) {
-                    portfolio_grid.shuffle('update');
-                    e.preventDefault();
-                    $('.portfolio-filters .filter').parent().removeClass('active');
-                    $(this).parent().addClass('active');
-                    portfolio_grid.shuffle('shuffle', $(this).attr('data-group') );
-                });
-    
-            }
-        }
-        // /Portfolio subpage filters
-    
-    
+    "use strict";    
         // Hide Mobile menu
         function mobileMenuHide() {
             var windowWidth = $(window).width(),
@@ -89,8 +64,7 @@
         // /Contact form validator
     
         //On Window load & Resize
-        $(window)
-            .on('load', function() { //Load
+        $(window).on('load', function() { //Load
                 // Animation on Page Loading
                 $(".preloader").fadeOut( 800, "linear" );
     
@@ -112,7 +86,7 @@
     
     
         // On Document Load
-        $(document).on('ready', function() {    
+        $(document).on('ready', function() {  
             // Mobile menu
             $('.menu-toggle').on("click", function () {
                 $('#site_header').addClass('animate');
@@ -137,35 +111,8 @@
             });
 
             $('.portfolio-item-img a').miniPreview({prefetch: 'none'});
-            
-    
-            // Initialize Portfolio grid
-            var $portfolio_container = $(".portfolio-grid");
-            $portfolio_container.imagesLoaded(function () {
-                portfolio_init(this);
-            });
-    
-            // Blog grid init
-            var $container = $(".blog-masonry");
-            $container.imagesLoaded(function(){
-                $container.masonry();
-            });
     
             customScroll();
-    
-            // Text rotation
-            $('.text-rotation').owlCarousel({
-                loop: true,
-                dots: false,
-                nav: false,
-                margin: 0,
-                items: 1,
-                autoplay: true,
-                autoplayHoverPause: false,
-                autoplayTimeout: 3800,
-                animateOut: 'animated-section-scaleDown',
-                animateIn: 'animated-section-scaleUp'
-            });
 
             /*==================================================================
             [ Typed text ]*/
@@ -183,59 +130,6 @@
             css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
             document.body.appendChild(css);
     
-            // Testimonials Slider
-            $(".testimonials.owl-carousel").imagesLoaded(function(){
-                $(".testimonials.owl-carousel").owlCarousel({
-                    nav: true, // Show next/prev buttons.
-                    items: 3, // The number of items you want to see on the screen.
-                    loop: false, // Infinity loop. Duplicate last and first items to get loop illusion.
-                    navText: false,
-                    autoHeight: true,
-                    margin: 25,
-                    responsive : {
-                        // breakpoint from 0 up
-                        0 : {
-                            items: 1,
-                        },
-                        // breakpoint from 480 up
-                        480 : {
-                            items: 1,
-                        },
-                        // breakpoint from 768 up
-                        768 : {
-                            items: 2,
-                        },
-                        1200 : {
-                            items: 2,
-                        }
-                    }
-                });
-            });
-    
-            // Clients Slider
-            $(".clients.owl-carousel").imagesLoaded().owlCarousel({
-                nav: true, // Show next/prev buttons.
-                items: 2, // The number of items you want to see on the screen.
-                loop: false, // Infinity loop. Duplicate last and first items to get loop illusion.
-                navText: false,
-                margin: 10,
-                autoHeight: true,
-                responsive : {
-                    // breakpoint from 0 up
-                    0 : {
-                        items: 2,
-                    },
-                    // breakpoint from 768 up
-                    768 : {
-                        items: 4,
-                    },
-                    1200 : {
-                        items: 5,
-                    }
-                }
-            });
-    
-    
             //Form Controls
             $('.form-control')
                 .val('')
@@ -247,58 +141,6 @@
                         $(this).parent('.form-group').removeClass('form-group-focus');
                     }
                 });
-    
-            // Lightbox init
-            $('body').magnificPopup({
-                delegate: 'a.lightbox',
-                type: 'image',
-                removalDelay: 300,
-    
-                // Class that is added to popup wrapper and background
-                // make it unique to apply your CSS animations just to this exact popup
-                mainClass: 'mfp-fade',
-                image: {
-                    // options for image content type
-                    titleSrc: 'title',
-                    gallery: {
-                        enabled: true
-                    },
-                },
-    
-                iframe: {
-                    markup: '<div class="mfp-iframe-scaler">'+
-                            '<div class="mfp-close"></div>'+
-                            '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-                            '<div class="mfp-title mfp-bottom-iframe-title"></div>'+
-                          '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
-    
-                    patterns: {
-                        youtube: {
-                          index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
-    
-                          id: null, // String that splits URL in a two parts, second part should be %id%
-                          // Or null - full URL will be returned
-                          // Or a function that should return %id%, for example:
-                          // id: function(url) { return 'parsed id'; }
-    
-                          src: '%id%?autoplay=1' // URL that will be set as a source for iframe.
-                        },
-                        vimeo: {
-                          index: 'vimeo.com/',
-                          id: '/',
-                          src: '//player.vimeo.com/video/%id%?autoplay=1'
-                        },
-                    },
-    
-                    srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
-                },
-    
-                callbacks: {
-                    markupParse: function(template, values, item) {
-                     values.title = item.el.attr('title');
-                    }
-                },
-            });
         });
     
     })(jQuery);
