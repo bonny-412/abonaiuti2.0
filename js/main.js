922c11e92633e78c4c0a592db81f2885bf71ad88
bonny-412 <bonny-412@users.noreply.github.com>
2021-01-24 23:21:12 +0100

    Commit

--- js/main.js
+++ js/main.js
@@ -1,23 +1,22 @@
 (function($) {
     "use strict";    
-        // Hide Mobile menu
-        function mobileMenuHide() {
-            var windowWidth = $(window).width(),
-                siteHeader = $('#site_header');
-    
-            if (windowWidth < 1025) {
-                siteHeader.addClass('mobile-menu-hide');
-                $('.menu-toggle').removeClass('open');
-                setTimeout(function(){
-                    siteHeader.addClass('animate');
-                }, 500);
-            } else {
-                siteHeader.removeClass('animate');
-            }
+    // Start Hide Mobile menu
+    function mobileMenuHide() {
+        var windowWidth = $(window).width(),
+            siteHeader = $('#site_header');
+        if (windowWidth < 1025) {
+            siteHeader.addClass('mobile-menu-hide');
+            $('.menu-toggle').removeClass('open');
+            setTimeout(function(){
+                siteHeader.addClass('animate');
+            }, 500);
+        } else {
+            siteHeader.removeClass('animate');
         }
-        // /Hide Mobile menu
+    }
+    // End Hide Mobile menu
     
-        // Custom scroll
+    // Start Custom scroll
         function customScroll() {
             var windowWidth = $(window).width();
             if (windowWidth > 1024) {
@@ -26,79 +25,69 @@
                 });
             } else {
                 $('.animated-section, .single-page-content').each(function() {
-                    $(this).perfectScrollbar('destroy');
+                $(this).perfectScrollbar('destroy');
+            });
+        }
+    }
+    // End Custom scroll
+    
+    // Start Contact form validator
+    $(function () {
+        $('#contact_form').validator();
+        $('#contact_form').on('submit', function (e) {
+            if (!e.isDefaultPrevented()) {
+                var url = "contact_form/contact_form.php";
+                $.ajax({
+                    type: "POST",
+                    url: url,
+                    data: $(this).serialize(),
+                    success: function (data) {
+                        var messageAlert = 'alert-' + data.type;
+                        var messageText = data.message;
+                        var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                        if (messageAlert && messageText) {
+                            $('#contact_form').find('.messages').html(alertBox);
+                            $('#contact_form')[0].reset();
+                        }
+                    }
+                });
+                return false;
+            }
+        });
+    });
+    // End Contact form validator
+    
+    //On Window load & Resize
+    $(window).on('load', function() { //Load
+        // Animation on Page Loading
+        $(".preloader").fadeOut( 800, "linear" );
+            // initializing page transition.
+            var ptPage = $('.animated-sections');
+            if (ptPage[0]) {
+                PageTransitions.init({
+                    menu: 'ul.main-menu',
                 });
             }
-        }
-        // /Custom scroll
-    
-        // Contact form validator
-        $(function () {
-    
-            $('#contact_form').validator();
-    
-            $('#contact_form').on('submit', function (e) {
-                if (!e.isDefaultPrevented()) {
-                    var url = "contact_form/contact_form.php";
-    
-                    $.ajax({
-                        type: "POST",
-                        url: url,
-                        data: $(this).serialize(),
-                        success: function (data)
-                        {
-                            var messageAlert = 'alert-' + data.type;
-                            var messageText = data.message;
-    
-                            var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-                            if (messageAlert && messageText) {
-                                $('#contact_form').find('.messages').html(alertBox);
-                                $('#contact_form')[0].reset();
-                            }
-                        }
-                    });
-                    return false;
-                }
+        }).on('resize', function() { //Resize
+            mobileMenuHide();
+            $('.animated-section').each(function() {
+                $(this).perfectScrollbar('update');
             });
+            customScroll();
         });
-        // /Contact form validator
-    
-        //On Window load & Resize
-        $(window).on('load', function() { //Load
-                // Animation on Page Loading
-                $(".preloader").fadeOut( 800, "linear" );
-    
-                // initializing page transition.
-                var ptPage = $('.animated-sections');
-                if (ptPage[0]) {
-                    PageTransitions.init({
-                        menu: 'ul.main-menu',
-                    });
-                }
-            })
-            .on('resize', function() { //Resize
-                 mobileMenuHide();
-                 $('.animated-section').each(function() {
-                    $(this).perfectScrollbar('update');
-                });
-                customScroll();
-            });
-    
-    
+
         // On Document Load
-        $(document).on('ready', function() {  
+        $(document).on('ready', function() {
             // Mobile menu
             $('.menu-toggle').on("click", function () {
                 $('#site_header').addClass('animate');
                 $('#site_header').toggleClass('mobile-menu-hide');
                 $('.menu-toggle').toggleClass('open');
             });
-    
             // Mobile menu hide on main menu item click
             $('.main-menu').on("click", "a", function (e) {
                 mobileMenuHide();
             });
-                
             // Sidebar toggle
             $('.sidebar-toggle').on("click", function () {
                 $('#blog-sidebar').toggleClass('open');
@@ -114,8 +103,7 @@
     
             customScroll();
 
-            /*==================================================================
-            [ Typed text ]*/
+            //Start Typed text
             var elements = document.getElementsByClassName('txt-rotate');
             for (var i=0; i<elements.length; i++) {
                 var toRotate = elements[i].getAttribute('data-rotate');
@@ -129,7 +117,8 @@
             css.type = "text/css";
             css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
             document.body.appendChild(css);
-    
+            //End Typed text
+
             //Form Controls
             $('.form-control')
                 .val('')
