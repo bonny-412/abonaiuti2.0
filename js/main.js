3c908f1a327eb7e7d89bc2d1d6b8678ab9766e35
bonny-412 <bonny.sviluppo@gmail.com>
2024-03-28 16:38:09 +0100

    Aggiunta funzione calcoloParametriDinamici

--- js/main.js
+++ js/main.js
@@ -31,6 +31,29 @@
     }
     //End function to DarkMode
 
+    function calcoloParametriDinamici() {
+        const progettiPersonali = 7;
+        const etaAnni = calcoloAnni('1997-12-27');
+        const esperienzaLavorativaAnni = calcoloAnni('2018-03-18');
+
+        $('#spanEta').text(etaAnni);
+        $('#spanAnniEsperienza').text(esperienzaLavorativaAnni + '+');
+        $('#spanProgettiPersonali').text(progettiPersonali + '+');
+    }
+
+    function calcoloAnni(dataDaCalcolare) {
+        const currentDate = new Date();
+        const data = new Date(dataDaCalcolare);
+        const anniInMilliseconds = currentDate - data;
+        //Converto i millisecondi in anni
+        const anni = anniInMilliseconds / (1000 * 60 * 60 * 24 * 365.25);
+        //Arrotondo per intero all'anno più vicino
+        const roundAge = Math.floor(anni);
+
+        console.log(roundAge);
+        return roundAge;
+    }
+
     // Start function to return submit
     const processForm = form => {
         const data = new FormData(form)
@@ -79,100 +102,102 @@
     $(window).on('load', function() { //Load
         // Animation on Page Loading
         $(".preloader").fadeOut( 800, "linear" );
-            // initializing page transition.
-            let ptPage = $('.animated-sections');
-            if (ptPage[0]) {
-                PageTransitions.init({
-                    menu: 'ul.main-menu',
-                });
+        // initializing page transition.
+        let ptPage = $('.animated-sections');
+        if (ptPage[0]) {
+            PageTransitions.init({
+                menu: 'ul.main-menu',
+            });
+        }
+    }).on('resize', function() { //Resize
+        mobileMenuHide();
+    });
+
+    // On Document Load
+    $(document).on('ready', function() {
+        var isDarkMode = true;
+
+        calcoloParametriDinamici();
+
+        // Mobile menu
+        $('.menu-toggle').on("click", function () {
+            $('#site_header').addClass('animate');
+            $('#site_header').toggleClass('mobile-menu-hide');
+            $('.menu-toggle').toggleClass('open');
+        });
+        // Mobile menu hide on main menu item click
+        $('.main-menu').on("click", "a", function (e) {
+            if($(this).attr('id') != 'darkMode') {
+                mobileMenuHide();
             }
-        }).on('resize', function() { //Resize
-            mobileMenuHide();
+        });
+        // Sidebar toggle
+        $('.sidebar-toggle').on("click", function () {
+            $('#blog-sidebar').toggleClass('open');
         });
 
-        // On Document Load
-        $(document).on('ready', function() {
-            var isDarkMode = true;
-
-            // Mobile menu
-            $('.menu-toggle').on("click", function () {
-                $('#site_header').addClass('animate');
-                $('#site_header').toggleClass('mobile-menu-hide');
-                $('.menu-toggle').toggleClass('open');
-            });
-            // Mobile menu hide on main menu item click
-            $('.main-menu').on("click", "a", function (e) {
-                if($(this).attr('id') != 'darkMode') {
-                    mobileMenuHide();
-                }
-            });
-            // Sidebar toggle
-            $('.sidebar-toggle').on("click", function () {
-                $('#blog-sidebar').toggleClass('open');
-            });
-
-            $('#darkMode').on('click', function() {
-                activeDarkMode(isDarkMode);
-                if(isDarkMode) {
-                    isDarkMode = false;
-                }else {
-                    isDarkMode = true;
-                }
-            });
-
-            $('#input-dark-mode').on('click', function() {
-                activeDarkMode(isDarkMode);
-                if(isDarkMode) {
-                    isDarkMode = false;
-                }else {
-                    isDarkMode = true;
-                }
-            });
-
-            init_pointer({
-                pointerColor: "#04b4e0",
-                ringSize: 15,
-                ringClickSize: 10
-            });
-    
-            //Start Typed text
-            let elements = document.getElementsByClassName('txt-rotate');
-            for (let i=0; i<elements.length; i++) {
-                let toRotate = elements[i].getAttribute('data-rotate');
-                let period = elements[i].getAttribute('data-period');
-                if (toRotate) {
-                    new TxtRotate(elements[i], JSON.parse(toRotate), period);
-                }
+        $('#darkMode').on('click', function() {
+            activeDarkMode(isDarkMode);
+            if(isDarkMode) {
+                isDarkMode = false;
+            }else {
+                isDarkMode = true;
             }
-            // INJECT CSS
-            let css = document.createElement("style");
-            css.type = "text/css";
-            css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-            document.body.appendChild(css);
-            //End Typed text
-
-            //Form Controls
-            $('.form-control').val('').on("focusin", function(){
-                $(this).parent('.form-group').addClass('form-group-focus');
-            }).on("focusout", function(){
-                if($(this).val().length === 0) {
-                    $(this).parent('.form-group').removeClass('form-group-focus');
-                }
-            });
-
-            const emailForm = document.querySelector('.contact-form')
-            if (emailForm) {
-                emailForm.addEventListener('submit', e => {
-                    if($('.btn-send').hasClass( "disabled" ) == false) {
-                        e.preventDefault();
-                        processForm(emailForm);
-                    }else {
-                        return false;
-                    }
-                });
-            }
-            $('.-close').click(modalClose);
-            
         });
+
+        $('#input-dark-mode').on('click', function() {
+            activeDarkMode(isDarkMode);
+            if(isDarkMode) {
+                isDarkMode = false;
+            }else {
+                isDarkMode = true;
+            }
+        });
+
+        init_pointer({
+            pointerColor: "#04b4e0",
+            ringSize: 15,
+            ringClickSize: 10
+        });
+
+        //Start Typed text
+        let elements = document.getElementsByClassName('txt-rotate');
+        for (let i=0; i<elements.length; i++) {
+            let toRotate = elements[i].getAttribute('data-rotate');
+            let period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+                new TxtRotate(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        let css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+        document.body.appendChild(css);
+        //End Typed text
+
+        //Form Controls
+        $('.form-control').val('').on("focusin", function(){
+            $(this).parent('.form-group').addClass('form-group-focus');
+        }).on("focusout", function(){
+            if($(this).val().length === 0) {
+                $(this).parent('.form-group').removeClass('form-group-focus');
+            }
+        });
+
+        const emailForm = document.querySelector('.contact-form')
+        if (emailForm) {
+            emailForm.addEventListener('submit', e => {
+                if($('.btn-send').hasClass( "disabled" ) == false) {
+                    e.preventDefault();
+                    processForm(emailForm);
+                }else {
+                    return false;
+                }
+            });
+        }
+        $('.-close').click(modalClose);
+        
+    });
     
     })(jQuery);
